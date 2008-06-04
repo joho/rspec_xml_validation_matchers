13834dcf9a2de52c9a57f89fe5a3d672a0488176
John Barton <jrbarton@gmail.com>
2008-06-04 17:26:57 +1000

    adding xsd support

--- lib/spec/rails/matchers/dtd.rb
+++ lib/spec/rails/matchers/dtd.rb
@@ -3,14 +3,30 @@ module Spec
     module Matchers
       def conform_to_dtd(filename)
         return simple_matcher("xml to confirm to dtd #{filename}") do |xml|
-          dtd = nil
-          File.open(filename, 'r') do |f|
-            dtd = XML::Dtd.new(f.read)
+          validate_from_xsd_or_dtd(xml, filename) do |file_contents|
+            XML::Dtd.new(file_contents)
           end
-
-          document = XML::Parser.string(xml).parse
-          document.validate(dtd)
         end
+      end
+      
+      def conform_to_xsd(filename)
+        return simple_matcher("xml to confirm to dtd #{filename}") do |xml|
+          validate_from_xsd_or_dtd(xml, filename) do |file_contents|
+            #raise file_contents.to_s
+            XML::Schema.from_string(file_contents)
+          end
+        end
+      end
+      
+      private
+      def validate_from_xsd_or_dtd(xml, filename)
+        dtd = nil
+        File.open(filename, 'r') do |f|
+          dtd = yield f.read
+        end
+
+        document = XML::Parser.string(xml).parse
+        !! document.validate(dtd)
       end
     end
   end
